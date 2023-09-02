f6b457b2099dc6bfa8cdf442ec0257aa90707150
GiovaniXX <giovani_chaves@hotmail.com>
2023-09-02 13:19:25 -0300

    Atualização de código...

--- src/formularios/Principal.java
+++ src/formularios/Principal.java
@@ -202,7 +202,7 @@ public class Principal extends javax.swing.JFrame {
                 .addComponent(jSeparator4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblSystemMessage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(141, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         lblFoto.getAccessibleContext().setAccessibleDescription("");
